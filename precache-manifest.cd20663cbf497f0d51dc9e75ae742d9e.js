5d02f1fb23f1a155e4f86f7613d4c570c8f32163
Armin Hafner <a_h82@gmx.net>
2020-02-08 16:07:10 +0100

    Updates

---
+++ precache-manifest.cd20663cbf497f0d51dc9e75ae742d9e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30ef17d0572c3ce42f40c10b3857bd65",
+    "url": "/markdown-previewer/index.html"
+  },
+  {
+    "revision": "d64a38aca6e26993f279",
+    "url": "/markdown-previewer/static/css/main.59601b5d.chunk.css"
+  },
+  {
+    "revision": "2f72c39be17ab9b81361",
+    "url": "/markdown-previewer/static/js/2.8d98e8c7.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/markdown-previewer/static/js/2.8d98e8c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d64a38aca6e26993f279",
+    "url": "/markdown-previewer/static/js/main.f723690d.chunk.js"
+  },
+  {
+    "revision": "424ab4b49e7473d5f412",
+    "url": "/markdown-previewer/static/js/runtime-main.75121bad.js"
+  }
+]);
